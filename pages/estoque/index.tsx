5ff81b9222d2abf39bf5ba10acdd954f7ff714ee
vihuro <vitorhugorodrigues93@gmail.com>
2023-08-09 17:31:06 -0300

    agora vai

--- pages/estoque/index.tsx
+++ pages/estoque/index.tsx
@@ -3,10 +3,11 @@ import Body from "../../components/table/Body"
 import Table from "../../components/table/storageFabrica/Table"
 
 export default function Estoque() {
+    const { Page, setToogleValue, toogleValue } = MenuBar();
     return (
         <>
-            <MenuBar />
-            <Body>
+            <Page />
+            <Body changeToogleAlterarSenha={setToogleValue} toogleCardAlterarSenha={toogleValue}>
                 <Table />
             </Body>
 
