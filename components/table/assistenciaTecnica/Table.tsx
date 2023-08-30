bb0dbf67ebd4b6bdb6c33e1148840a944dbc021b
Vitor Hugo <vitorhugorodrigues93@gmail.com>
2023-08-30 08:10:05 -0300

    passando para o pc

---
+++ components/table/assistenciaTecnica/Table.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react"
+import Api from "../../../service/api/assistenciaTecnica/Assistencia"
+import style from "./style.module.css";
+import { BsInfoCircle } from "react-icons/bs"
+
+interface ordemServicoProps {
+    descricao: string,
+    id: number,
+    maquinas: maquinaProps[],
+    status: string
+}
+interface maquinaProps {
+    maquinaId: string,
+    maquina: string
+}
+
+export default function Table() {
+    const [data, setData] = useState<ordemServicoProps[]>([]);
+    useEffect(() => {
+        console.log("aqui")
+        Api.get("")
+            .then(res => setData(res.data))
+            .catch(err => console.log(err))
+    }, [])
+    return (
+        <main className={style.container_body} >
+            <main className={style.container_table} >
+                <table className={style.table} >
+                    <thead>
+                        <tr>
+                            <th>
+                                ID
+                            </th>
+                            <th>
+                                Descrição
+                            </th>
+                            <th>
+                                Status
+                            </th>
+                            <th>
+                                Maq.
+                            </th>
+                            <th>
+                                Info
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data && data.length > 0 && (
+                            data.map((item, index) => (
+                                <tr>
+                                    <td>{item.id}</td>
+                                    <td>{item.descricao}</td>
+                                    <td>{item.status}</td>
+                                    <td>{item.maquinas.length}</td>
+                                    <td>
+                                        <BsInfoCircle />
+                                    </td>
+                                </tr>
+                            ))
+                        )}
+                    </tbody>
+                </table>
+            </main>
+        </main>
+
+    )
+}
--- service/api/assistenciaTecnica/Assistencia.tsx
+++ service/api/assistenciaTecnica/Assistencia.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL:"http://192.168.0.105:8080/api/v1/estoque/matriz/grm"
+    baseURL:"http://localhost:5184/api/v1/ordem-servico"
 })
-
 export default Api;
