48ccac787f551da261045f3e8f556fb62e2f423c
Vitor Hugo <vitorhugorodrigues93@gmail.com>
2023-11-27 01:39:25 -0300

    não sei

--- service/api/matriz/estoque-grm.tsx
+++ service/api/matriz/estoque-grm.tsx
@@ -6,7 +6,7 @@ import { parseCookies } from "nookies";
     
 // })
 const Api = axios.create({
-    baseURL:"http://192.168.1.87:8080/api/v1/estoque/matriz/grm",
+    baseURL:"http://192.168.0.113:8080/api/v1/estoque/matriz/grm",
     
 })
 Api.interceptors.request.use((request)=>{
