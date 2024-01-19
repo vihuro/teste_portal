37743137c58bd33357500214816129301c8a9a2f
vihuro <vitorhugorodrigues93@gmail.com>
2024-01-19 12:40:46 -0300

    adicionando card para tipo de aquisição do maquina no cliente, Assistec

--- components/table/assistenciaTecnica/clientAssistencia/filterMaquinaDisponivel/cardConfirmAddMaquina/CardConfirmAddMaquina.tsx
+++ components/table/assistenciaTecnica/clientAssistencia/filterMaquinaDisponivel/cardConfirmAddMaquina/CardConfirmAddMaquina.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import RadioButton from "../../../../../UI/input/radio/RadioButton";
 import styles from "./style.module.css";
 
 interface CardConfirmAddMaquinaProps {
@@ -6,6 +8,18 @@ interface CardConfirmAddMaquinaProps {
   addMaquinaInCliente: Function;
 }
 export default function CardConfirmAddMaquina({}: CardConfirmAddMaquinaProps) {
+  const { Radio } = RadioButton();
+  enum TipoAquisicao {
+    VENDA = 1,
+    EMPRESTIVO = 2,
+  }
+  const [valueTipoAquisicao, setValueTipoAquisicao] = useState<TipoAquisicao>(
+    TipoAquisicao.VENDA
+  );
+
+  const blockedInputDataSugerida =
+    valueTipoAquisicao === TipoAquisicao.EMPRESTIVO ? false : true;
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapContainer}>
@@ -14,9 +28,38 @@ export default function CardConfirmAddMaquina({}: CardConfirmAddMaquinaProps) {
         </header>
         <main className={styles.body}>
           <div className={styles.wrapBody}>
-            <div className={styles.inputDataSugerida}>
-              <input title="Data sugerida de retorno" type="date" />
-              <label htmlFor="">DATA SUGERIDA</label>
+            <div className={styles.containerRadioButton}>
+              <Radio
+                color="green"
+                name="tipoAquisicao"
+                id="rdbVenda"
+                text="VENDA"
+                checked={valueTipoAquisicao === TipoAquisicao.VENDA ?? true}
+                onChange={() => setValueTipoAquisicao(TipoAquisicao.VENDA)}
+              />
+              <Radio
+                color="green"
+                name="tipoAquisicao"
+                id="rdbEmprestimo"
+                text="EMPRÉSTIMO"
+                checked={
+                  valueTipoAquisicao === TipoAquisicao.EMPRESTIVO ?? true
+                }
+                onChange={() => setValueTipoAquisicao(TipoAquisicao.EMPRESTIVO)}
+              />
+            </div>
+            <div
+              className={`${styles.inputDataSugerida} ${
+                blockedInputDataSugerida && styles["--block"]
+              }`}
+            >
+              <input
+                readOnly={blockedInputDataSugerida}
+                id="txtDataSugerida"
+                title="Data sugerida de retorno"
+                type="date"
+              />
+              <label htmlFor="txtDataSugerida">DATA SUGERIDA</label>
             </div>
           </div>
         </main>
